00564826103b69c8d8a1c68921a7c38d59bd4b1a
hungdaoqc <hungd@criticalmass.com>
2020-06-13 22:43:00 +0700

    Webdriver helper: issue - failed Search

---
+++ configs/webdriver.conf.js
@@ -0,0 +1,47 @@
+const { setHeadlessWhen } = require("@codeceptjs/configure");
+const dotenv = require("dotenv").config();
+const sharedConfig = require("./codecept_shared.js");
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+
+//setHeadlessWhen(process.env.HEADLESS);
+let helpers = {
+  WebDriver: {
+    browser: "firefox",
+    url: "https://www.amazon.com",
+    restart: false,
+    show: true,
+    timeouts: {
+      script: 60000,
+      pageLoad: 10000,
+    },
+    //browser: "chrome",
+    smartWait: 5000,
+    waitForTimeout: 5000,
+    desiredCapabilities: {
+      chromeOptions: {
+        args: [
+          "--incognito",
+          "--disable-extensions",
+          "--disable-gpu",
+          "--no-sandbox",
+          "--disable-dev-shm-usage",
+        ],
+      },
+    },
+  },
+  ChaiWrapper: {
+    require: "codeceptjs-chai",
+  },
+};
+
+let wdio = {
+  enabled: true,
+  services: ["selenium-standalone"],
+};
+
+sharedConfig.helpers = helpers;
+sharedConfig.plugins.wdio = wdio;
+
+exports.config = sharedConfig;
