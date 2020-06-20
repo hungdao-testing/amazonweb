454594f766e547d54bb1f6827d41a7caf1901bb6
Hung Dao <51052135+hungdao-testing@users.noreply.github.com>
2020-06-21 00:28:26 +0700

    Update SUT.js

--- SUT.js
+++ SUT.js
@@ -6,7 +6,8 @@ module.exports = {
 
     testSpecFolder: path.join(projectBasedDir, '/tests/specs/**/*.spec.js'),
     outputFolder: path.join(projectBasedDir,'output'),
-    bootstrapFile: "./../run_server.js", // if use dynamic function to get path (e.g. process.cwd()) get error could not find bootstrapFile
+    //Issue for configuring dynamic bootstrap: https://github.com/codecept-js/CodeceptJS/issues/2437
+    bootstrapFile: "./../run_server.js",
     configFolder: path.join(projectBasedDir, '/configs'),
     IObject: path.join(projectBasedDir,'/steps_file.js'),
     pageFactory: path.join(projectBasedDir,'/tests/pageFactory.js')
